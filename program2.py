12e06a5de0b9a8d5114c1c3ec1fe69ea15084553
Jagdish Prajapati <151916824+jagdish575@users.noreply.github.com>
2024-02-15 15:40:05 +0530

    first commit

--- program2.py
+++ program2.py
@@ -5,3 +5,26 @@ class Solution(object):
         :rtype: int
         """
         pass
+def romanToInt(s: str) -> int:
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    prev_value = 0
+    
+    for char in reversed(s):
+        value = roman_dict[char]
+        if value < prev_value:
+            result -= value
+        else:
+            result += value
+        prev_value = value
+    
+    return result
+
+
+roman_numeral = input("Enter a Roman numeral: ")
+
+
+result = romanToInt(roman_numeral)
+
+print("The integer representation of", roman_numeral, "is:", result)
+
